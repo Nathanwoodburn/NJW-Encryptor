f95e93bfc71b6a3f38013f56fa94ee3acec6b92d
Nathan Woodburn <nathan.woodburn2611@gmail.com>
2020-10-14 10:42:16 +1100

    Add project files.

---
+++ NJW Encryptor/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Encryptor;
+
+namespace NJW_Encryptor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string key;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            key = "password123";
+            richTextBox1.Focus();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open1 = new OpenFileDialog();
+            open1.Filter = "txt|*.txt|all files|*.*";
+            
+                if (richTextBox1.Modified)
+            {
+                DialogResult result1 = MessageBox.Show("Do you want to continue? You will lose text currently in textbox.", "Continue?", MessageBoxButtons.YesNoCancel, MessageBoxIcon.None);
+                if (result1 == DialogResult.Yes)
+                {
+                    if (open1.ShowDialog() == DialogResult.OK)
+                    {
+                        StreamReader stream1 = new StreamReader(open1.FileName);
+                        richTextBox1.Text = stream1.ReadToEnd();
+                        stream1.Dispose();
+                    }
+                }
+                
+            }
+            else
+            {
+                if (open1.ShowDialog() == DialogResult.OK)
+                {
+                    StreamReader stream2 = new StreamReader(open1.FileName);
+                    richTextBox1.Text = stream2.ReadToEnd();
+                    stream2.Dispose();
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+richTextBox2.Text = Encryptor.Encryptor.Encrypt(richTextBox1.Text, key);
+            }
+            catch (Exception)
+            {
+                richTextBox2.Text = "Error Please check key";
+                
+            } 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox2.Text = Encryptor.Encryptor.Decrypt(richTextBox1.Text, key);
+            }
+            catch (Exception)
+            {
+                richTextBox2.Text = "Error Please check key";
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Your key is:\n"+key,"View Key");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            frmchangekey frmchangekey1 = new frmchangekey(key);
+            if (frmchangekey1.ShowDialog() == DialogResult.OK)
+            {
+                key = frmchangekey1.changedkey;
+
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save1 = new SaveFileDialog();
+            save1.Filter = "txt|*.txt|All files|*.*";
+            if (save1.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter stream3 = new StreamWriter(save1.FileName);
+                stream3.Write(richTextBox2.Text);
+                stream3.Dispose();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            richTextBox1.Focus();
+        }
+    }
+}
---
+++ NJW Encryptor/frmchangekey.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NJW_Encryptor
+{
+    public partial class frmchangekey : Form
+    {
+        public string changedkey { get; set; }
+        //public string ReturnValue2 { get; set; }
+        string key;
+        public frmchangekey(string ckey)
+        {
+            InitializeComponent();
+            key = ckey;
+        }
+
+        private void frmchangekey_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = key;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            changedkey = textBox1.Text;
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.UseSystemPasswordChar == true)
+                textBox1.UseSystemPasswordChar = false;
+            else
+                textBox1.UseSystemPasswordChar = true;
+
+          
+         
+        }
+    }
+}
